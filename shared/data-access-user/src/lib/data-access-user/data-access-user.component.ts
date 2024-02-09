bb28cf5866162ea634ba2d2f99b49e4925270f0c
Sergei Nagaitsev <x64vs32x@mail.ru>
2024-02-09 09:14:19 +0300

    feat: data-access-user OnPush

--- shared/data-access-user/src/lib/data-access-user/data-access-user.component.ts
+++ shared/data-access-user/src/lib/data-access-user/data-access-user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -7,5 +7,6 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   templateUrl: './data-access-user.component.html',
   styleUrl: './data-access-user.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DataAccessUserComponent {}
